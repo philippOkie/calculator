54506474773effa69f28f50b19c6f7787f39780a
philippOkie <kriukphilipp@gmail.com>
2023-05-28 15:25:52 +0300

    updated the calc's design, +/- button was added

--- code.js
+++ code.js
@@ -2,9 +2,12 @@ const button = document.getElementById('myButton');
 const display = document.getElementById('display');
 
 
-button.addEventListener('click', displayValue())
+button.addEventListener('click', displayValue)
+
 function displayValue() {
     const buttonValue = button.innerHTML;
+    
+    buttonValue += button.innerHTML;
     display.value = buttonValue; 
 }
 
